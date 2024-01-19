0ecd4137603f7b4b89be20809a1187f2c03134fd
NoManPlay <819134128@qq.com>
2024-01-19 11:15:32 +0800

    add video on md & update responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,23 @@ import Image from 'next/image'
 export default function Home() {
   const date = new Date('2024-08-20 00:00:00')
   return (
-    <main className="h-screen bg-[url('/img_bg_m.png')] md:bg-[url('/img_bg.png')] bg-[length:100%] md:bg-[length:100%] bg-no-repeat w-full">
+    <main className="h-screen">
+      <video
+        className="hidden md:block md:absolute md:w-full"
+        poster="/img_bg.png"
+        autoPlay
+        loop
+        muted
+        object-fit="fill"
+      >
+        <source src="/video_bg.mp4" type="video/mp4"></source>
+      </video>
       <div className="absolute md:pl-[52vw] md:pt-[5vw] text-center">
         <div className="relative w-[100vw] h-[50vw] md:w-[30vw] md:h-[15vw]">
-          {
-            //视频
-            <video className="hidden md:block" poster="/img_bg.png" autoPlay loop muted>
-              <source src="/video_bg.mp4" type="video/mp4"></source>
-            </video>
-          }
           <Image src="/logo.png" alt="black_myth" layout="fill" priority objectFit="contain" />
         </div>
-        <p className="mt-[10vw] md:mt-0 text-[8vw] md:text-[3vw]">直面天命</p>
-        <div className="tnum tabular-nums text-[10vw] md:text-[4vw] text-[#B09B7C]">
+        <p className="relative mt-[10vw] md:mt-0 text-[8vw] md:text-[3vw]">直面天命</p>
+        <div className="relative tnum tabular-nums text-[10vw] md:text-[4vw] text-[#B09B7C]">
           <Countdown date={date}>
             {({timeLeft, isValidDate}) =>
               isValidDate ? (
