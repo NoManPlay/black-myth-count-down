3d76caf0bdfddd838313d6be7f43d6cb5aa05b3c
NoManPlay <819134128@qq.com>
2024-04-26 10:18:03 +0800

    update ps url

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const GITHUB_URL = 'https://github.com/NoManPlay/black-myth-count-down'
 
 export const STEAM_STORE_URL = 'https://store.steampowered.com/app/2358720/Black_Myth_Wukong/'
-export const PLAYSTATION_STORE_URL = 'https://www.playstation.com/games/black-myth-wukong/'
+export const PLAYSTATION_STORE_URL = 'https://www.playstation.com/zh-hant-hk/games/black-myth-wukong/'
